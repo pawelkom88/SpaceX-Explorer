270d4d796b88add9ed711d289f25bb883d8ec267
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-13 16:55:00 +0100

    feat: Add number of entries dropdown component

---
+++ src/components/number-of-entries-dropdown/NumOfEntriesDropdown.tsx
@@ -0,0 +1,30 @@
+import { Container, InputLabel, MenuItem, Select } from "@mui/material";
+
+interface NumOfEntriesDropdownProps {
+  limit: number;
+  onLimitChange: (limit: number) => void;
+}
+
+export function NumOfEntriesDropdown({
+  limit,
+  onLimitChange,
+}: NumOfEntriesDropdownProps) {
+  return (
+    <Container sx={{ textAlign: "center" }}>
+      <InputLabel sx={{ marginBottom: 1 }} id="limit">
+        Number of missions displayed in a grid
+      </InputLabel>
+      <Select
+        sx={{ minWidth: 100 }}
+        onChange={(e) => onLimitChange(Number(e.target.value))}
+        labelId="limit"
+        id="select"
+        value={limit}
+      >
+        <MenuItem value={6}>6</MenuItem>
+        <MenuItem value={12}>12</MenuItem>
+        <MenuItem value={24}>24</MenuItem>
+      </Select>
+    </Container>
+  );
+}
