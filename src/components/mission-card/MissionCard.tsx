25bd455e548578b41373929312cb9ea3a6fe5301
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-13 10:53:28 +0100

    refactor: Improve mission card component

--- src/components/mission-card/MissionCard.tsx
+++ src/components/mission-card/MissionCard.tsx
@@ -1,53 +1,123 @@
+import { DateRange, Rocket } from "@mui/icons-material";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import {
-  Button,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   List,
   ListItem,
+  ListItemText,
   Typography,
 } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 import { CARD_HEIGHT, CARD_WIDTH } from "../../utils/constants";
 import { getYouTubeID } from "../../utils/helpers";
+import classes from "./mission-card.module.css";
+
+const cardProps = {
+  height: CARD_HEIGHT * 1.3,
+  maxWidth: CARD_WIDTH,
+  boxShadow: "2px 1px 1px 1px rgba(255, 255, 255, 0.2)",
+};
 
 interface MissionCardProps {
-  launches: any;
+  launch: any;
 }
 
-export function MissionCard({ launches }: MissionCardProps) {
+export function MissionCard({ launch }: MissionCardProps) {
   return (
-    <Card key={launches.id} sx={{ maxWidth: CARD_WIDTH }}>
-      <CardMedia
-        component="iframe"
-        sx={{ height: CARD_HEIGHT / 2.5 }}
-        src={`https://www.youtube.com/embed/${getYouTubeID(launches.links.video_link)}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {launches.mission_name}
-        </Typography>
-        <Typography p={2} variant="body2" sx={{ color: "text.primary" }}>
-          {launches.details
-            ? `${launches.details.slice(0, 100)} ...`
-            : "No details available"}
-        </Typography>
-        <List>
-          <ListItem>{launches.rocket.rocket_name}</ListItem>
-          <ListItem>{launches.rocket.rocket_type}</ListItem>
-        </List>
-        <List>
-          <ListItem>{launches.launch_year}</ListItem>
-          <ListItem>{launches.launch_success ? "Success" : "Failure"}</ListItem>
-        </List>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Link className={classes["card-link"]} to={`/mission/${launch.id}`}>
+      <Card className={classes.card} key={launch.id} sx={cardProps}>
+        <VideoEmbed videoLink={launch.links.video_link} />
+        <CardContent sx={{ padding: 3 }}>
+          <MissionTitle title={launch.mission_name} />
+          <MissionDetails details={launch.details} />
+          <CardList icon={<Rocket />} text={launch.rocket.rocket_name} />
+          <List component="ul">
+            <CardList icon={<DateRange />} text={launch.launch_year} />
+            <LaunchStatus success={launch.launch_success} />
+          </List>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+}
+
+interface VideoEmbedProps {
+  videoLink: string;
+}
+
+function VideoEmbed({ videoLink }: VideoEmbedProps) {
+  return (
+    <CardMedia
+      component="iframe"
+      sx={{ height: CARD_HEIGHT / 2.5 }}
+      src={`https://www.youtube.com/embed/${getYouTubeID(videoLink)}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  );
+}
+
+interface MissionTitleProps {
+  title: string;
+}
+
+function MissionTitle({ title }: MissionTitleProps) {
+  return (
+    <Typography gutterBottom variant="h5" component="div">
+      {title}
+    </Typography>
+  );
+}
+
+interface MissionDetailsProps {
+  details: string;
+}
+
+function MissionDetails({ details }: MissionDetailsProps) {
+  return (
+    <Typography variant="body2" sx={{ color: "text.primary" }}>
+      {details ? `${details.slice(0, 100)} ...` : "No details available"}
+    </Typography>
+  );
+}
+
+interface CardListProps {
+  icon: React.ReactElement;
+  text: string;
+}
+
+function CardList({ icon, text }: CardListProps) {
+  return (
+    <List component="ul">
+      <ListItem>
+        {icon}
+        <ListItemText sx={{ marginLeft: 1, fontStyle: "italic" }}>{text}</ListItemText>
+      </ListItem>
+    </List>
+  );
+}
+
+interface LaunchStatusProps {
+  success: boolean;
+}
+
+function LaunchStatus({ success }: LaunchStatusProps) {
+  return (
+    <ListItem>
+      <ListItemText
+        sx={{
+          marginLeft: 1,
+          fontStyle: "italic",
+        }}
+      >
+        {success ? <ThumbUpIcon /> : <ThumbDownIcon />}
+        {success ? "Success" : "Failure"}
+      </ListItemText>
+    </ListItem>
   );
 }
