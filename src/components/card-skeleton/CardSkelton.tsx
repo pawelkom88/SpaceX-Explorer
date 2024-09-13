576bfc473a1ecb73e6c725be95a837be351c3827
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-13 16:54:39 +0100

    chore:  Add number prop

--- src/components/card-skeleton/CardSkelton.tsx
+++ src/components/card-skeleton/CardSkelton.tsx
@@ -1,10 +1,12 @@
 import { Skeleton } from "@mui/material";
 import { CARD_HEIGHT, CARD_WIDTH } from "../../utils/constants";
 
-export  function CardSkelton() {
-  return   <>
-  {Array.from({ length: 25 }).map(() => (
-    <Skeleton variant="rectangular" width={CARD_WIDTH} height={CARD_HEIGHT} />
-  ))}
-</>;
+export function CardSkelton({ length = 6 }: { length?: number }) {
+  return (
+    <>
+      {Array.from({ length }).map(() => (
+        <Skeleton variant="rectangular" width={CARD_WIDTH} height={CARD_HEIGHT} />
+      ))}
+    </>
+  );
 }
