e61bb2fe1b97d003dbe6db79e069739730161091
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-13 16:55:36 +0100

    chore: Add custom hook to handle pagination

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function usePagination(initialLimit: number) {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(initialLimit);
+
+  const offset = (page - 1) * limit;
+
+  const handleLimitChange = (newLimit: number) => {
+    setLimit(newLimit);
+    setPage(1);
+  };
+
+  return { page, setPage, limit, handleLimitChange, offset };
+}
