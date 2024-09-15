e3a9d203e14f60817cef9cd8c6d42e6ee6c17c4a
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-15 10:59:57 +0100

    chore: move cardProps to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,9 @@ export const sortOptions = {
   mission_name: "mission_name",
   launch_date_local: "launch_date_local",
 } as const;
+
+export const cardProps = {
+  height: CARD_HEIGHT * 1.4,
+  maxWidth: CARD_WIDTH,
+  boxShadow: "2px 1px 1px 1px rgba(255, 255, 255, 0.2)",
+};
